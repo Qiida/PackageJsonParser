b7a52901802f84f37fa11f3566fc2bd67b2597e1
Qida Ye <q.ye@tu-braunschweig.de>
2024-07-29 19:45:56 +0200

    initial commit

--- out/artifacts/FullyAutomatedPackageJsonParser_jar/PackageJsonParser.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-mode con: cols=150 lines=9999
-CLS
-cmd /k java -jar PackageJsonParser.jar -paths c:\src\BK25_Workspace\Packages\TCI c:\src\BK25_Workspace\Packages\KWI -duration 30 -unit minutes
